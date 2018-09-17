f3ecb74ee2ba4f4748c37b06efaafd7aaeae7689
Dmitry Avvakumov <avvakumov@tochka.com>
2018-09-17 11:25:32 +0500

    Initial Commit

---
+++ testPaging/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testPaging
+//
+//  Created by Dmitriy Avvakumov on 17.09.2018.
+//  Copyright © 2018 Dmitriy Avvakumov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ testPaging/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  testPaging
+//
+//  Created by Dmitriy Avvakumov on 17.09.2018.
+//  Copyright © 2018 Dmitriy Avvakumov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
